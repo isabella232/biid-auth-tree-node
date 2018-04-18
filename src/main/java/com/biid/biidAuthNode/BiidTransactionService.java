e2bf2f4ec688bfa3e6aef597b46ad31a571dfeba
Sergei Shushkevich <s.shushkevich@gmail.com>
2018-04-18 19:13:43 +0300

    (BIIDC-800) Add proper Java implementation of BiidTransactionService

--- src/main/java/com/biid/biidAuthNode/BiidTransactionService.java
+++ src/main/java/com/biid/biidAuthNode/BiidTransactionService.java
@@ -1,8 +1,39 @@
 package com.biid.biidAuthNode;
 
-import javax.net.ssl.SSLContext;
+import com.biid.api.service.integrator.api.AuthorizationApi;
+import com.biid.api.service.integrator.api.TransactionsApi;
+import com.biid.api.service.integrator.gen.ApiClient;
+import com.biid.api.service.integrator.gen.ApiException;
+import com.biid.api.service.integrator.model.AccessToken;
+import com.biid.api.service.integrator.model.CreateTransactionRequest;
+import com.biid.api.service.integrator.model.TransactionInfo;
+import com.nimbusds.jose.EncryptionMethod;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWEAlgorithm;
+import com.nimbusds.jose.JWEHeader;
+import com.nimbusds.jose.crypto.RSAEncrypter;
+import com.nimbusds.jwt.EncryptedJWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.squareup.okhttp.OkHttpClient;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.KeyStoreException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.UUID;
+import javax.net.ssl.SSLContext;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 
 /**
  * Biid transaction service based on api-biid-integrator library
@@ -11,65 +42,78 @@ import java.util.UUID;
  */
 public class BiidTransactionService {
 
-    public void sendAuthTransaction(String username, String entityKey, String entityAppKey, String callbackUrl) {
+    public static final String PUBLIC_API_URL = "https://api.test-biid.com";
+    public static final String AUDIENCE = "https://api.test-biid.com";
+    public static final String GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+    public static final String JWE_ALGORITHM = "RSA1_5";
+    public static final String JWE_ENCRYPTION_METHOD = "A256GCM";
+    public static final String SDK_PUBLIC_KEY_URL = "https://biid-keys.s3.amazonaws.com/sdk/sdk_public_key.der";
+    public static final String INTEGRATOR_SITE = "http://demo.biid.com";
+    public static final String INTEGRATOR_USERNAME = "jdoe";
+    public static final String INTEGRATOR_LANG = "en";
+
+    public void sendAuthTransaction(String username, String entityKey, String entityAppKey, String callbackUrl)
+            throws ApiException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException,
+                    MalformedURLException, IOException, InvalidKeySpecException, JOSEException {
         CreateTransactionRequest transactionRequest = new CreateTransactionRequest();
-        transactionRequest.expirationDate = new Date().format("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        transactionRequest.type = "AUTH";
-        transactionRequest.username = username;
-        transactionRequest.callback = callbackUrl;
-        transactionRequest.actions = ["ACCEPT", "REJECT"];
-        TransactionInfo info = new TransactionInfo()
+        transactionRequest.setExpirationDate(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                .format(new Date(new Date().getTime() + 24 * 60 * 60 * 1000)));
+        transactionRequest.setType(CreateTransactionRequest.TypeEnum.AUTH);
+        transactionRequest.setUsername(username);
+        transactionRequest.setCallback(callbackUrl);
+        transactionRequest.setActions(Arrays.asList("ACCEPT", "REJECT"));
+        transactionRequest.setAssuranceLevel(CreateTransactionRequest.AssuranceLevelEnum.L1);
+        TransactionInfo info = new TransactionInfo();
         info.put("title", "Authenticate");
         info.put("description", "Authenticate on site");
-        transactionRequest.info = info;
-        TransactionsApi transactionsApi = new TransactionsApi(getApiClient(entityKey, entityAppKey));
-        transactionsApi.uploadAndSign(transactionRequest)
+        info.put("location", Arrays.asList(2.154007, 41.390205));
+        transactionRequest.setInfo(info);
+        TransactionsApi transactionsApi = new TransactionsApi(getApiClient(entityAppKey, entityKey));
+        transactionsApi.uploadAndSign(transactionRequest);
     }
 
-    ApiClient getApiClient(String selectedEntityAppKey, String eid) {
-        try {
-            String clientToken = generateClientToken(selectedEntityAppKey, eid)
-            ApiClient apiClient = new ApiClient()
-            apiClient.basePath = getAudience()
-            OkHttpClient httpClient = new OkHttpClient()
-            def sslContext = SSLContext.getInstance("SSL")
+    private ApiClient getApiClient(String entityAppKey, String eid)
+            throws ApiException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException,
+                    MalformedURLException, IOException, InvalidKeySpecException, JOSEException {
+        String clientToken = generateClientToken(entityAppKey, eid);
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(PUBLIC_API_URL);
+        
+        OkHttpClient httpClient = new OkHttpClient();
+        TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
+            public boolean isTrusted(X509Certificate[] chain, String authType) {
+                return true;
+            }
+        };
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        httpClient.setSslSocketFactory(sslContext.getSocketFactory());
+        apiClient.setHttpClient(httpClient);
 
-            sslContext.init(null, [Holders.grailsApplication.mainContext.biidTrustManager] as TrustManager[], null)
-            def sslSocketFactory = sslContext.getSocketFactory()
-            httpClient.setSslSocketFactory(sslSocketFactory)
+        AuthorizationApi authorizationApi = new AuthorizationApi(apiClient);
+        AccessToken accessToken = authorizationApi.token(GRANT_TYPE, clientToken);
+        apiClient.setAccessToken(accessToken.getAccessToken());
 
-            apiClient.setHttpClient(httpClient)
-            AuthorizationApi authorizationApi = new AuthorizationApi(apiClient)
-
-            def accessToken = authorizationApi.token(grantType, clientToken)
-            apiClient.accessToken = accessToken.accessToken
-            return apiClient
-        } catch (ex) {
-            log.error("Could not create access token ${ex.message}")
-            return null
-        }
+        return apiClient;
     }
 
-    String generateClientToken(String selectedEntityAppKey, String eid) {
-        def now = new Date()
-        def builder = new JWTClaimsSet.Builder().audience(audience)
-                .issuer(selectedEntityAppKey ?: entityAppKey)
-                .issueTime(now).expirationTime(now + 1).subject("http://demo.biid.com")
-                .jwtID(UUID.randomUUID().toString()).claim("hl", "en")
-
-        if (eid) {
-            builder.claim("eid", eid)
+    private String generateClientToken(String entityAppKey, String eid)
+            throws NoSuchAlgorithmException, MalformedURLException, IOException,
+                    InvalidKeySpecException, JOSEException {
+        Date now = new Date();
+        JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder().audience(AUDIENCE).issuer(entityAppKey)
+                .issueTime(now).expirationTime(new Date(now.getTime() + 24 * 60 * 60 * 1000))
+                .jwtID(UUID.randomUUID().toString()).subject(INTEGRATOR_SITE)
+                .claim("hl", INTEGRATOR_LANG).claim("usr", INTEGRATOR_USERNAME);
+        if (eid != null) {
+            builder.claim("eid", eid);
         }
 
-        def jws = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), builder.build())
-        jws.sign(new MACSigner(Holders.config.com.biid.security.internalJwt.secret))
-
-        def jwe = new JWEObject(new JWEHeader.Builder(
-                JWEAlgorithm.parse(Holders.config.com.biid.security.jwe.algorithm),
-                EncryptionMethod.parse(Holders.config.com.biid.security.jwe.encryptionMethod))
-                .contentType("JWT").build(), new Payload(jws))
-        jwe.encrypt(new RSAEncrypter(sdkPublicKey))
-        return jwe.serialize()
+        JWEHeader header = new JWEHeader.Builder(JWEAlgorithm.parse(JWE_ALGORITHM), EncryptionMethod.parse(JWE_ENCRYPTION_METHOD))
+                .contentType("JWT").build();
+        EncryptedJWT jwe = new EncryptedJWT(header, builder.build());
+        jwe.encrypt(new RSAEncrypter((RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(
+                new X509EncodedKeySpec(IOUtils.toByteArray(new URL(SDK_PUBLIC_KEY_URL))))));
+        return jwe.serialize();
     }
 
 }
