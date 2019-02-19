ec51ac920990aa930dee781d751a58db95017526
Frank Gasparovic <frank.gasparovic@forgerock.com>
2019-02-19 15:04:30 -0800

    6.5 Support

--- src/main/java/com/biid/biidAuthNode/BiidAuthNodeDecision.java
+++ src/main/java/com/biid/biidAuthNode/BiidAuthNodeDecision.java
@@ -17,9 +17,9 @@
 
 package com.biid.biidAuthNode;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.assistedinject.Assisted;
 import com.sun.identity.shared.debug.Debug;
-import org.forgerock.guava.common.collect.ImmutableList;
 import org.forgerock.json.JsonValue;
 import org.forgerock.openam.auth.node.api.*;
 import org.forgerock.openam.core.CoreWrapper;
